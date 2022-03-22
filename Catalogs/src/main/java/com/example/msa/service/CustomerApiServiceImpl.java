d4a2d10146ffaa1b41760500aa15a7aa6feba6b6
Asha <ejdd9630@gmail.com>
2022-03-22 21:10:07 +0900

    유레카 서버에 클라이언트 Catalogs 등록하기

--- Catalogs/src/main/java/com/example/msa/service/CustomerApiServiceImpl.java
+++ Catalogs/src/main/java/com/example/msa/service/CustomerApiServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class CustomerApiServiceImpl implements CustomerApiService{
 
@@ -13,8 +15,17 @@ public class CustomerApiServiceImpl implements CustomerApiService{
 	
 	
 	@Override
+	@HystrixCommand(fallbackMethod="getCustomerDetailFallback")
 	public String getCustomerDetail(String customerId) {
-		return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
+			//restTemplate = Client(Web browser가 아님) 
+										//직접 요청은 loadbalancing이 아니다.(분산X), restTemplate이 분산해야한다.
+												//서버 리스트를 적는다.(어플리케이션 설정에 등록)
+		return restTemplate.getForObject("http://customer/customers/" + customerId, String.class);
+	}
+	
+	public String getCustomerDetailFallback(String customerId, Throwable ex) {
+		System.out.println("Error:"+ex.getMessage());
+		return "고객 정보 조회가 지연되고 있습니다.";
 	}
 
 }
